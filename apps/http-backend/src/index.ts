53592b73725c5220b66b4e5e723999ab6f98d7ec
vinod kumar <vinodrao835@gmail.com>
2025-11-27 09:55:31 +0530

    Built the truborepo where http-backend & ws-backend are pushing data to db & nextjs app pulling the data from the db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,10 +3,30 @@ import express from 'express';
 import { prisma } from '@repo/prisma/client';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from HTTP Backend!');
+console.log('Starting HTTP Backend...');
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+    console.log('Received signup request');
+    try {
+        const {username, password} = req.body;
+    
+        // create users in the database
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password,
+            },
+        }); 
+
+        return res.status(201).json({ message: 'User created successfully', userId: user.id });
+    } catch (error) {
+        console.log('Error during signup:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(PORT, () => {
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,16 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+import { prisma } from '@repo/prisma/client';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', async (ws: WebSocket) => {
+    await prisma.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    });
+
+    console.log('New client connected');
+});
